2b4cd3590d726741dd964e90aefc5b1e60524cd1
stinlove81 <stinlove81@gmail.com>
2026-02-02 23:50:14 +0900

    Add files via upload

--- scraper_mtpl.py
+++ scraper_mtpl.py
@@ -41,7 +41,6 @@ def run_mtpl_engine():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
